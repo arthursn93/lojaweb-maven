69b4064d39643889bfd6567ade850b0fa9b17a04
Arthur da Silva Nunes <arthurnunes@brq.com>
2020-07-31 18:39:41 -0300

    Gerando um webapp em Maven

---
+++ src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -0,0 +1,24 @@
+package br.com.alura.maven.lojaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.maven.Produto;
+ 
+@WebServlet(urlPatterns = {"/contato"})
+public class ContatoServlet extends HttpServlet{
+		
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Produto p = new Produto("salgadinho", 1.52);
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><h2>Entre em contato conosco!</h2></html>");
+		writer.close();
+	}
+}
